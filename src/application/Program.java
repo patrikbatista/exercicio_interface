a69f0891de479bc1d1ca0d79984769a3cebff2d7
patrik <pcr_batista@hotmail.com>
2022-01-04 19:11:55 -0300

    finaliza projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,48 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entites.Contract;
+import model.entites.Installment;
+import model.service.ContractService;
+import model.service.PaypalService;
+
 public class Program {
+	
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Enter contract data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		System.out.print("Date (dd/MM/yyyy): ");
+		Date date = sdf.parse(sc.next());
+		System.out.print("Contract value: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		System.out.print("Enter number of installments: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PaypalService());
+		
+		contractService.processContract(contract, n);
+		
+		System.out.println("Installments:");
+		for (Installment x : contract.getInstallments()) {
+			System.out.println(x);
+		}
+		
+		sc.close();
+		
+	}
 
 }
